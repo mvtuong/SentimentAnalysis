f1ac10e1dc5fedc96c024d3a7f276c0f92299547
mvtuong <mvtuong90@gmail.com>
2016-02-23 15:47:49 -0700

    fixing bugs

--- app.py
+++ app.py
@@ -1,30 +1,36 @@
+from flask import Blueprint
+main = Blueprint('main', __name__)
 import json
 from engine import SentimentAnalysis 
 from flask import Flask, request
 
-app = Flask(__name__)
-
-@app.route("/")
+@main.route("/", methods = ['GET'])
 def hello():
-    return "Hello World!"
+    return "Hello World 1!"
  
-@app.route("/predict/", methods = ["POST"])
+@main.route('/predict/', methods = ['POST'])
 def get_predict():
     if request.method == 'POST':
+        with open("log.txt", "w") as text_file:
+            text_file.write("data: " + str(request.data))
         if 0 < len(request.data) < 5000:
             text = str(request.data)
+            
             rating = sa.get_predict_ratings(text)
+            
             response = {"message": "success", "value": rating[0]}
             return json.dumps(response)
         else:
             response = {"message": "error", "value": "something wrong with the input"}
             return json.dumps(response)
  
+def create_app():
+    global sa
+    sa = SentimentAnalysis()
+    app = Flask(__name__)
+    app.register_blueprint(main)
+    return app
  
 if __name__ == "__main__":
-    global sa
-    sa = SentimentAnalysis()    
-    
-    #app.debug = True
-    #app.run( host="127.0.0.1", port=int("8888") )
-    app.run(debug=True)
+    app = create_app()
+    app.run()
